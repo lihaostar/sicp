90f560359803c81d30a36f7607ec1f81316e6000
lihaostar <lihaostar@msn.com>
2019-04-20 23:40:42 +0800

    Create 1.17.rkt

---
+++ 1.17.rkt
@@ -0,0 +1,18 @@
+(define (* b n)
+    (mul-iter b n 0))
+
+(define (mul-iter b n a)
+    (cond ((= n 0)
+            a)
+          ((even? n)
+            (mul-iter (double b)
+                       (halve n)
+                       a))
+          ((odd? n)
+            (mul-iter b
+                       (- n 1)
+                       (+ b a)))))
+(define (double x)
+  (+ x x))
+(define (halve x)
+  (/ x 2))
